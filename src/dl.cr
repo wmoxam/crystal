1bbad82dabddc86ff347fac3c1f3962a319e480c
Wesley Moxam <wes@4ormat.com>
2016-02-07 23:29:00 -0500

    Remove formatting change

--- src/dl.cr
+++ src/dl.cr
@@ -15,10 +15,10 @@ lib LibDL
   fun dlopen(path : LibC::Char*, mode : LibC::Int) : Void*
 
   ifdef darwin || openbsd
-    RTLD_NEXT       = Pointer(Void).new(-1)
-    RTLD_DEFAULT    = Pointer(Void).new(-2)
-    RTLD_SELF       = Pointer(Void).new(-3)
-    RTLD_MAIN_ONLY  = Pointer(Void).new(-5)
+    RTLD_NEXT      = Pointer(Void).new(-1)
+    RTLD_DEFAULT   = Pointer(Void).new(-2)
+    RTLD_SELF      = Pointer(Void).new(-3)
+    RTLD_MAIN_ONLY = Pointer(Void).new(-5)
   else
     RTLD_NEXT    = Pointer(Void).new(-1)
     RTLD_DEFAULT = Pointer(Void).new(0)
